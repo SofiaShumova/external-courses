a87a593d5212770b67b5c4fb02d381574b467556
SofiaShumova <sonya.shumova@yandex.ru>
2020-03-12 00:41:03 +0300

    feat(ex5): completed tasks

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,26 @@
+const Calculator = {
+  result: 0,
+  add: function(x = 0) {
+    Calculator.result += x
+    return Calculator.add
+  },
+  subtract: function(x = 0) {
+    Calculator.result -= x
+    return Calculator.subtract
+  },
+  divide: function(x = 1) {
+    Calculator.result *= x
+    return Calculator.divide
+  },
+  multiply: function(x = 1) {
+    Calculator.result /= x
+    return Calculator.multiply
+  },
+  getResult: function() {
+    return this.result
+  },
+  reset: function() {
+    return Calculator.result = 0
+  }
+}
+module.exports = Calculator
