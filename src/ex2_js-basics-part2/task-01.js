ae01a2405b53c83d8105a1ea6333eba78aa21655
SofiaShumova <sonya.shumova@yandex.ru>
2020-03-12 12:57:51 +0300

    fix(ex2): task01 added check for NaN

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,7 +1,10 @@
 function checkType(value) {
 	if (typeof(value) === "string" || typeof(value) === "number") {
+		if (typeof(value) === "number" && isNaN(value)) {
+			return undefined;
+		}
 		return typeof(value);
 	}
 	return undefined;
-} 
+}
 module.exports = checkType
