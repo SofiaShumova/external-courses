7941f4488cf35a17712093518d3044e3ed2e9e5b
SofiaShumova <sonya.shumova@yandex.ru>
2020-04-11 19:34:48 +0300

    fix(ex6): format documents

--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -1,10 +1,12 @@
 function filter(array, callback) {
 	let newArray = []
+	
 	for (let i = 0; i < array.length; i++) {
 		if (callback(array[i], i, array)) {
 			newArray.push(array[i])
 		}
 	}
+
 	return newArray
 }
 module.exports = filter
