8474dfdd87d75d07e4fe1aeeb7691f7ff91c65ad
SofiaShumova <sonya.shumova@yandex.ru>
2020-04-14 03:21:24 +0300

    fix(ex12): added new animation

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -6,39 +6,45 @@ let arrayImage = [
     "https://bipbap.ru/wp-content/uploads/2018/06/3ee998952c2016c72a9ad131db1ea00d.jpg",
     "https://bipbap.ru/wp-content/uploads/2018/06/background-backgrounds-floral-flower-Favim.com-2429619.jpg"
 ]
-let slider ={
+let slider = {
     state: {
-        firstImage: 0,
-        secondImage: 1,
-        thirdImage: 2
+        firstImage: -1,
+        secondImage: 0,
+        thirdImage: 1
     },
-    increment: function(){
-        if(slider.state["thirdImage"]<arrayImage.length-1){
-            for(let key in slider.state){
-                slider.state[key]+=1
+    increment: function () {
+        if (slider.state["thirdImage"] < arrayImage.length) {
+            for (let key in slider.state) {
+                slider.state[key] += 1
             }
-            updateViewSlider() 
-        }             
+            updateViewSlider()
+        }
     },
-    decrement:function(){
-        if(slider.state["firstImage"]>0){
-            for(let key in slider.state){
-                slider.state[key]-=1
+    decrement: function () {
+        if (slider.state["firstImage"] > -1) {
+            for (let key in slider.state) {
+                slider.state[key] -= 1
             }
-            updateViewSlider() 
-        }             
+            updateViewSlider()
+        }
     },
-    end:false,
+    end: false,
     start: true
 }
-function updateViewSlider(){
-    for(let prop in slider.state){
+function updateViewSlider() {
+    for (let prop in slider.state) {
         let elem = document.getElementById(prop);
-        elem.src=arrayImage[slider.state[prop]]
-        elem.setAttribute("class", "animationClass");
+        if (prop === "firstImage" && slider.state[prop] === -1 ||
+            prop === "thirdImage" && slider.state[prop] === arrayImage.length) {
+            elem.style.visibility = "hidden"
+        } else {
+            elem.style.visibility = "visible"
+            elem.src = arrayImage[slider.state[prop]]
+        }
+        elem.setAttribute("class", prop === "secondImage" ? "animationCenter" : "animationClass");
     }
-    setTimeout(()=>{
-        for(let prop in slider.state){
+    setTimeout(() => {
+        for (let prop in slider.state) {
             setTimeout(document.getElementById(prop).setAttribute("class", ""), 1000)
         }
     }, 300)
