34b46aa669cf9fa7dbb282bb606b1c07f127e844
SofiaShumova <sonya.shumova@yandex.ru>
2020-04-20 01:40:31 +0300

    fix(kanban): editing style and submenu

--- src/kanban/submenu.js
+++ src/kanban/submenu.js
@@ -4,13 +4,14 @@ let arrow = document.getElementById("arrow")
 menu.setAttribute("id", "accountMenu")
 
 account.addEventListener("click", displayAccountMenu)
+account.appendChild(menu)
 
 function displayAccountMenu(){
-    if(!document.getElementById("accountMenu")){
-        account.appendChild(menu)
+    if(document.getElementById("accountMenu").style.display==="none"){
+        menu.style.display="block";
         arrow.style.transform = "rotate(180deg)"
     } else{
-        account.removeChild(menu)
+        menu.style.display="none";
         arrow.style.transform = "rotate(360deg)"
     }
 }
@@ -18,6 +19,7 @@ function displayAccountMenu(){
 function createMenu(){
     let items = ["Account", "Tasks", "Edit", "Settings", "Log out"];
     let list = document.createElement("ul");
+    list.style.display="none";
 
     for(let item of items){
         let liItem = document.createElement("li");
