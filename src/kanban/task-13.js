db1822eb29a0dcce46d9c2cee8a11c061d99a30a
SofiaShumova <sonya.shumova@yandex.ru>
2020-04-21 22:50:32 +0300

    feat(ex13): completed task

---
+++ src/kanban/task-13.js
@@ -0,0 +1,137 @@
+const dataMock = [
+    {
+        title: 'backlog',
+        issues: []
+    },
+    {
+        title: 'ready',
+        issues: []
+    },
+    {
+        title: 'progress',
+        issues: []
+    },
+    {
+        title: 'finished',
+        issues: []
+    }
+]
+
+class Issue {
+    constructor(id, name) {
+        this.id = "task" + id;
+        this.name = name;
+    }
+}
+
+function addNewIssue(name) {
+    dataMock[0].issues.push(new Issue(dataMock[0].issues.length+1, name))
+}
+
+function addIssue(title, name){
+    let index = getIndex(title);
+    let issue = getIssue(index-1, name);
+    removeIssue(index-1, name);
+    dataMock[index].issues.push(issue);
+}
+
+function getIndex(title){
+    return dataMock.findIndex((elem, index)=>{
+        return elem.title == title
+    });
+}
+function getIssue(index, name){
+    let findIndex = dataMock[index].issues.findIndex((elem)=>{
+        return elem.name === name;
+    })
+    return dataMock[index].issues[findIndex]
+}
+
+function removeIssue(index, name){
+    let removeIndex = dataMock[index].issues.findIndex((elem)=>{
+        return elem.name === name;
+    });
+    dataMock[index].issues.splice(removeIndex,1);
+    document.getElementById(dataMock[index].title).getElementsByClassName("content")[0].getElementsByClassName("task")[removeIndex].remove()
+}
+
+let buttonsAdd = document.getElementsByClassName("footerTab")
+let contents = document.getElementsByClassName("content")
+
+for(let button of buttonsAdd){
+    button.addEventListener("click", actionAdd);
+}
+
+function actionAdd() {
+    let title = this.parentElement.id;
+    let index = getIndex(title);
+
+    let taskBlock = document.createElement("div");
+    taskBlock.setAttribute("class", "task");
+    
+    let inputText = document.createElement("input");    
+    inputText.setAttribute("type", "text");
+    inputText.setAttribute("id", "newTaskInput");
+
+    taskBlock.appendChild(inputText);
+    
+    if(index>0){
+        let datalist = getDataList(index-1);
+        datalist.setAttribute("id", "list");
+        inputText.appendChild(datalist);
+        inputText.setAttribute("list", "list")
+    }
+    
+    inputText.addEventListener("blur", handlerInput)
+    inputText.addEventListener("keypress", function (event) {
+        if (event.keyCode == 13) {
+            inputText.blur();
+        }
+    })
+
+    contents[index].appendChild(taskBlock)
+}
+
+function getDataList(index){
+    let datalist = document.createElement("datalist");
+
+    for(let issue of dataMock[index].issues){
+        let option = document.createElement("option")
+        option.setAttribute("value", issue.name)
+        datalist.appendChild(option)
+    }
+
+    return datalist
+}
+
+function handlerInput() {
+    let title = this.parentElement.parentElement.parentElement.id
+    if(this.value === ""){
+        this.parentElement.remove()
+        return
+    } else if( getIndex(title)>0){
+        if(getIssue(getIndex(title)-1, this.value) == undefined){
+            this.parentElement.remove()
+            return  
+        }
+    }
+
+    if(title==="backlog"){
+        addNewIssue(this.value);
+    } else{
+        addIssue(title, this.value);
+    }
+    
+    let p = document.createElement("p").innerHTML = this.value;
+    document.querySelector("#newTaskInput").replaceWith(p)
+    updateLocalStorage();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('datamock', JSON.stringify(dataMock));
+}
+
+
+
+
+
