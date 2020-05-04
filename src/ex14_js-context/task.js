7896bb66465bf0d4ff7278cc68ef3956ae8f0aa1
SofiaShumova <sonya.shumova@yandex.ru>
2020-05-05 01:19:23 +0300

    fix(ex14): edit polybind

--- src/ex14_js-context/task.js
+++ src/ex14_js-context/task.js
@@ -1,8 +1,5 @@
 function bind(func, context, ...param) {
     return function (...args) {
-        context['polybind'] = func;
-        let resultFunction = context['polybind'](...param.concat(args));
-        delete context['polybind'];
-        return resultFunction;
+        return func.call(context, ...param.concat(args) );
     }
 }
